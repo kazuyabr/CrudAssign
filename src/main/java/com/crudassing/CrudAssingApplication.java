fb8b7dc1f99d989bf3493b89c5c7d9c5c266eaa6
Sulivan <sulivan.lineage2@gmail.com>
2020-12-22 18:05:07 -0300

    primeiro commit

---
+++ src/main/java/com/crudassing/CrudAssingApplication.java
@@ -0,0 +1,13 @@
+package com.crudassing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAssingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAssingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crudassing/CrudAssingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudassing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAssingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
